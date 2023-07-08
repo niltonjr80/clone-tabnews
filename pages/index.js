1e4360de493683c68201d50b822b145f23115916
Nilton Junior <14946185+niltonjr80@users.noreply.github.com>
2023-07-08 22:27:14 +0000

    Adiciona a pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+function Home() {
+    return (
+    <>
+        <h1>
+            Nossa maior fraqueza está em desistir.
+        </h1>
+        <h1>
+            O caminho mais certo de vencer é tentar mais uma vez.
+        </h1>    
+        <p> 
+            Thomas Edison
+        </p>
+    </>
+    )
+}
+
+export default Home;
