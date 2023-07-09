e57eb7db545f77dd4580e53bcb8353799de86591
Nilton Junior <14946185+niltonjr80@users.noreply.github.com>
2023-07-09 04:30:14 +0000

    Adiciona scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,13 @@
 function Home() {
-    return (
+  return (
     <>
-        <h1>
-            Nossa maior fraqueza está em desistir. 
-        </h1>
-        <h1>
-           Então nunca desista! Pois o caminho mais certo de vencer é tentar mais uma vez.
-        </h1>    
-        <p> 
-            Thomas Edison
-        </p>
+      <h1>Nossa maior fraqueza está em desistir.</h1>
+      <h1>
+        Então nunca desista! Pois o caminho mais certo de vencer é tentar mais uma vez.
+      </h1>
+      <p>Thomas Edison</p>
     </>
-    )
+  );
 }
 
 export default Home;
