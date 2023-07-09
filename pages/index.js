f409db4ecfcccf8adf4fd0c13903510c9a443111
Nilton Junior <14946185+niltonjr80@users.noreply.github.com>
2023-07-09 04:44:35 +0000

    adiciona `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ function Home() {
     <>
       <h1>Nossa maior fraqueza está em desistir.</h1>
       <h1>
-        Então nunca desista! Pois o caminho mais certo de vencer é tentar mais uma vez.
+        Então nunca desista! Pois o caminho mais certo de vencer é tentar mais
+        uma vez.
       </h1>
     </>
   );
