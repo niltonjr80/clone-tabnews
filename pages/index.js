e09c52fff5ef42c5d1f3ff891010c39f861978e1
Nilton Junior <14946185+niltonjr80@users.noreply.github.com>
2023-07-09 01:27:20 +0000

    Frase atualizada

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,10 @@ function Home() {
     return (
     <>
         <h1>
-            Nossa maior fraqueza está em desistir.
+            Nossa maior fraqueza está em desistir. 
         </h1>
         <h1>
-            O caminho mais certo de vencer é tentar mais uma vez.
+           Então nunca desista! Pois o caminho mais certo de vencer é tentar mais uma vez.
         </h1>    
         <p> 
             Thomas Edison
