270a9d4b81e3e110fae0731d0614967f1b8ba7cf
Nilton Junior <14946185+niltonjr80@users.noreply.github.com>
2024-04-19 00:35:07 +0000

    add `/api/v1/status/` endpoint

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <>
-      <h1>Nossa maior fraqueza está em desistir.</h1>
+      <h1>A maior fraqueza está em desistir.</h1>
       <h1>
         Então nunca desista! Pois o caminho mais certo de vencer é tentar mais
         uma vez.
