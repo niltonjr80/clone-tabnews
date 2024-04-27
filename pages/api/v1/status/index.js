ad6e93a5b757ab9e1445ba24f303c4342b1a6062
Nilton Junior <14946185+niltonjr80@users.noreply.github.com>
2024-04-27 20:52:50 +0000

    Finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,48 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result);
-  response.status(200).json({ chave: "São acima da média!" });
+  const updateAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("Show server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "show max_connections;",
+  );
+
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+  /*
+  const databaseOpenedConnectionsResult = await database.query(
+    "SELECT * FROM pg_stat_activity WHERE datname = 'local_db';",
+  );
+   */
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  console.log("Conexões abertas: " + databaseOpenedConnectionsValue);
+
+  /*
+    Existe um consenso que uma API em Rest não deve responder com
+    chaves em lowerCamelCase, mas sim, no formato snake_case onde
+    cada palavra deve ser separada por um underscore
+  */
+  response.status(200).json({
+    updated_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
