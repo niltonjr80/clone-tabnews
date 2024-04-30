9b35d568e723d33a96f7a993c05fc9e4f5d41c0b
Nilton Junior <npjunior2001@gmail.com>
2024-04-30 21:18:46 +0000

    Add  for debug

--- infra/database.js
+++ infra/database.js
@@ -10,6 +10,15 @@ async function query(queryObject) {
     ssl: process.env.NODE_ENV === "development" ? false : true,
   });
 
+  console.log("Credenciais do Postgres: ", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
+  });
+
   try {
     await client.connect();
     const result = await client.query(queryObject);
